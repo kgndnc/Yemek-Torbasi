2d4b70eab7d8209c1930fc054f9e8e9ab752cb80
kgndnc <hkagandnc@gmail.com>
2022-01-20 22:52:32 +0300

    index page box layout fixed (row and cols using flex)

--- index.php
+++ index.php
@@ -7,42 +7,47 @@ include 'functions.php';
 
 $page_name = "Anasayfa";
 include 'layout/top.php';
-echo $_SESSION['user_id'];
 ?>
 
 
 <main class="container pt-3">
-    <div class="restau-box">
-        Burger Place
-        <div class="restau-img mt-2">
-            <img src="img/burger.jpg" alt="">
+
+    <div class="row row-cols-3">
+        <div class="restau-box col">
+            Burger Place
+            <div class="restau-img mt-2">
+                <img src="img/burger.jpg" alt="">
+            </div>
+            <button type="button" class="btn btn-primary mt-3"  onclick="document.location='restau-burger.php'">Ürünleri Listele</button>
         </div>
-        <button type="button" class="btn btn-primary mt-3"  onclick="document.location='restau-burger.php'">Ürünleri Listele</button>
+
+        <div class="restau-box col">
+            Pizza Place
+            <div class="restau-img mt-2">
+                <img src="img/pizza.png" alt="">
+            </div>
+            <button type="button" class="btn btn-primary mt-5"  onclick="document.location='restau-pizza.php'">Ürünleri Listele</button>
+        </div>
+
+        <div class="restau-box col">
+            Patisserie
+            <div class="restau-img mt-2">
+                <img src="img/burger.jpg" alt="">
+            </div>
+            <button type="button" class="btn btn-primary mt-3"  onclick="document.location='restau-patiss.php'">Ürünleri Listele</button>
+        </div>
     </div>
 
-    <div class="restau-box">
-        Pizza Place
-        <div class="restau-img mt-2">
-            <img src="img/pizza.png" alt="">
+    <div class="row row-cols-3">
+        <div class="restau-box">
+            Steakhouse
+            <div class="restau-img mt-2">
+                <img src="img/burger.jpg" alt="">
+            </div>
+            <button type="button" class="btn btn-primary mt-2"  onclick="document.location='restau-steakhouse.php'">Ürünleri Listele</button>
         </div>
-        <button type="button" class="btn btn-primary mt-5"  onclick="document.location='restau-pizza.php'">Ürünleri Listele</button>
     </div>
 
-    <div class="restau-box">
-        Patisserie
-        <div class="restau-img mt-2">
-            <img src="img/burger.jpg" alt="">
-        </div>
-        <button type="button" class="btn btn-primary mt-3"  onclick="document.location='restau-patiss.php'">Ürünleri Listele</button>
-    </div>
-
-    <div class="restau-box">
-        Steakhouse
-        <div class="restau-img mt-2">
-            <img src="img/burger.jpg" alt="">
-        </div>
-        <button type="button" class="btn btn-primary mt-2"  onclick="document.location='restau-steakhouse.php'">Ürünleri Listele</button>
-    </div>
 </main>
 
 <?php
